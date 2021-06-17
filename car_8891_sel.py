76a8481946930f99a6cbb774e517eb01694cfc21
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2021-06-17 21:18:25 +0800

    update

---
+++ car_8891_sel.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import time
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option('display.unicode.east_asian_width', True)
+driver = webdriver.Chrome('./chromedriver')
+driver.maximize_window()
+time.sleep(1)
+df = pd.DataFrame(columns=['Title', 'Describe', 'Price', 'Year', 'Distance'])
+for x in range(1, 4):
+    url = f'https://auto.8891.com.tw/?page={x}'
+    driver.get(url)
+    time.sleep(5)
+
+    for i in range(1, 41):
+        title = driver.find_element_by_xpath(
+            f'//*[@id="search-result"]/a[{i}]/div/div[2]/div[1]/span')
+        # print(title.text)
+
+        describe = driver.find_element_by_xpath(
+            f'//*[@id="search-result"]/a[{i}]/div/div[2]/div[2]')
+        # print(describe.text)
+
+        price = driver.find_element_by_xpath(
+            f'//*[@id="search-result"]/a[{i}]/div/div[3]/span[1]/b')
+        # print(price.text)
+
+        year = driver.find_element_by_xpath(
+            f'//*[@id="search-result"]/a[{i}]/div/div[3]/span[2]/b')
+        # print(year.text)
+
+        distance = driver.find_element_by_xpath(
+            f'//*[@id="search-result"]/a[{i}]/div/div[3]/span[3]')
+        # print(distance.text)
+        df = df.append({'Title': title.text, 'Describe': describe.text, 'Price': price.text,
+                       'Year': year.text, 'Distance': distance.text}, ignore_index=True)
+        
+
+print(df)
+df.to_csv('car.csv',encoding='utf-8')
+driver.close()
--- ptt.py
+++ ptt.py
@@ -6,9 +6,9 @@ import jieba.posseg
 import numpy as np
 import pandas as pd
 import operator
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"}
-
 
 def parse_html(url, headers):
     return BeautifulSoup(requests.get(url, headers=headers).text, 'lxml')
