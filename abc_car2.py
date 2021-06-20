89a64daef01caac682ae1094f272103431249c1a
shaunchuang <72535297+shaunchuang@users.noreply.github.com>
2021-06-20 19:55:15 +0800

    update

--- abc_car2.py
+++ abc_car2.py
@@ -1,14 +1,14 @@
 import asyncio
 from pyppeteer import launch
 from bs4 import BeautifulSoup
+import time
 
 async def main():
     # 開啟瀏覽器程序
     browser = await launch()
-    # 用瀏覽器連到 https://pythonclock.org/ 網站
     page = await browser.newPage()
     await page.goto('https://www.abccar.com.tw/search?tab=1&SearchType=1&OrderByField=0&page=1')    
-
+    time.sleep(3)
     html_doc = await page.content()
     soup = BeautifulSoup(html_doc, 'lxml')
 
---
+++ abc_car3.py
@@ -0,0 +1,4 @@
+
+headers={'cookie': '_gcl_au=1.1.1728982988.1624089038; __gads=ID=70384f1bba64682e:T=1624089037:S=ALNI_MaR4p9PPy2_PlilD7HCz9ni9a8exg; _ga=GA1.3.1085617352.1624089037; __BWfp=c1624089042902xd364ed052; _gid=GA1.3.2115392437.1624176184; _fbp=fb.2.1624176889577.974609533; abc_session=80nlnao0r7pljpqau4l9ggnecccs9srr; _gat_UA-76403875-5=1'}
+url='https://www.abccar.com.tw/car/1440564?car_source=index-top-dealer'
+
